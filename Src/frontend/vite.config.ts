4a00caa6ab27a4b635bb063775f4b1df299b476b
athiphat67 <athiphat.sun@dome.tu.ac.th>
2026-04-15 02:59:15 +0700

    feat: change path

---
+++ Src/frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  root: './',
+  plugins: [react()],
+  server: {
+    watch: {
+      // บอก Vite ว่าไม่ต้องไปสแกนหรือเฝ้าดูการเปลี่ยนแปลงในโฟลเดอร์ venv
+      ignored: ['**/venv/**'] 
+    }
+  },
+  optimizeDeps: {
+    // ป้องกันไม่ให้ Vite เข้าไปหา Dependencies ในฝั่ง Python
+    entries: ['index.html', 'Src/**/*.{ts,tsx}']
+  }
+})
