a91828652866ee5ee0dd87755f0cd1d116c91b25
athiphat67 <athiphat.sun@dome.tu.ac.th>
2026-04-19 00:04:27 +0700

    feat: update api

--- Src/frontend/api/main.py
+++ Src/frontend/api/main.py
@@ -25,7 +25,11 @@ else:
 from database.database import RunDatabase
 db = RunDatabase()
 from psycopg2.extras import RealDictCursor
-import supabase
+from supabase import create_client, Client
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 app = FastAPI(title="Nakkhutthong API")
 
