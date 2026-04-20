3bf94b08cb8d09ae51fd4c8d7cb3dce80eff9291
athiphat67 <athiphat.sun@dome.tu.ac.th>
2026-04-20 08:22:48 +0700

    feat: update frontend

--- Src/frontend/api/main.py
+++ Src/frontend/api/main.py
@@ -100,7 +100,7 @@ app = FastAPI(title="Nakkhutthong API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://cn-240-data-science-for-signal-git-74908c-athiphat67s-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
